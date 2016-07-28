85a187835218fa2ffe36896dc252f7cdcea29d7a
HellscreamCat <723924718@qq.com>
2016-07-28 20:24:40 +0800

    分期订单

---
+++ src/main/java/com/yufeng/controller/FastConsumerLoanOrderController.java
@@ -0,0 +1,46 @@
+package com.yufeng.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yufeng.entity.FastConsumerLoanOrder;
+import com.yufeng.entity.FastConsumerLoanOrderCommodity;
+import com.yufeng.service.impl.FastConsumerLoanOrderServiceImpl;
+
+/**
+ * 快速消费品分期订单控制层
+ * @author dh
+ */
+
+@RestController
+@RequestMapping("/FastConsumerLoanOrder")
+public class FastConsumerLoanOrderController {
+	@Autowired
+	private FastConsumerLoanOrderServiceImpl fastConsumerLoanOrderServiceImpl;
+	
+	//查询银行卡信息(code)
+	@RequestMapping("/getFastConsumerLoanOrder")
+	public Map<String,Object> getFastConsumerLoanOrder(String order_id){
+		return fastConsumerLoanOrderServiceImpl.getFastConsumerLoanOrder(order_id);
+	   }
+		
+	//新建银行卡信息
+	@RequestMapping("/insertFastConsumerLoanOrder")
+	public String insertFastConsumerLoanOrder(String json){
+		return fastConsumerLoanOrderServiceImpl.insertFastConsumerLoanOrder(json);
+	}
+		
+	//银行卡信息修改
+	@RequestMapping("/updateFastConsumerLoanOrder")
+	public String updateFastConsumerLoanOrder(String json){
+		return fastConsumerLoanOrderServiceImpl.updateFastConsumerLoanOrder(json);
+	}
+		
+
+}
