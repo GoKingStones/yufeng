b99e471043be131920e912d0bdf642a55fd2398a
HellscreamCat <723924718@qq.com>
2016-08-18 20:36:38 +0800

    sql语句完善

--- src/main/sql/用户家庭联系人历史.sql
+++ src/main/sql/用户家庭联系人历史.sql
@@ -1,20 +1,19 @@
-DROP TABLE IF EXISTS user_family_info_history_table;
 CREATE TABLE `user_family_info_history_table` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `internal_code` varchar(100) DEFAULT NULL,
-  `credentialsType` varchar(2) DEFAULT NULL COMMENT '证件类型',
-  `credentialsNumber` varchar(50) DEFAULT NULL COMMENT '证件号码',
-  `name` varchar(50) DEFAULT NULL COMMENT '姓名',
+  `internal_code` varchar(255) NOT NULL,
+  `credentialsType` varchar(2) NOT NULL COMMENT '证件类型',
+  `credentialsNumber` varchar(50) NOT NULL COMMENT '证件号码',
+  `name` varchar(50) NOT NULL COMMENT '姓名',
   `gender` varchar(2) DEFAULT NULL COMMENT '性别',
   `mailingAddress` varchar(200) DEFAULT NULL COMMENT '通讯地址',
   `organization` varchar(100) DEFAULT NULL COMMENT '工作单位',
-  `phone` varchar(11) DEFAULT NULL COMMENT '手机号码',
+  `phone` varchar(11) NOT NULL COMMENT '手机号码',
   `mailbox` varchar(100) DEFAULT NULL COMMENT '电子邮箱',
-  `relation` varchar(2) DEFAULT NULL COMMENT '与主体关系',
+  `relation` varchar(2) NOT NULL COMMENT '与主体关系',
   `highestEducationSchool` varchar(100) DEFAULT NULL COMMENT '最高学历院校',
   `highestEducation` varchar(100) DEFAULT NULL COMMENT '最高学历',
-  `foundTime` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '新建时间',
+  `foundTime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '新建时间',
   `updateTime` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-  `delete_operator` varchar(50) DEFAULT NULL COMMENT '删除操作者',
+  `delete_operator` varchar(50) NOT NULL COMMENT '删除操作者',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- src/main/sql/用户金融账户信息.sql
+++ src/main/sql/用户金融账户信息.sql
@@ -1,15 +1,13 @@
---创建金融账户信息表
-DROP TABLE IF EXISTS user_financial_account_info_table;
 CREATE TABLE `user_financial_account_info_table` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `internal_code` varchar(100) DEFAULT NULL COMMENT '内码',
+  `internal_code` varchar(50) NOT NULL COMMENT '内码',
   `signId` varchar(100) DEFAULT NULL COMMENT '信息标识ID',
-  `financialConsumeAccountId` varchar(100) DEFAULT NULL COMMENT '金融消费账户ID',
-  `financialConsumeCompany` varchar(100) DEFAULT NULL COMMENT '金融消费公司',
-  `financialConsumeAccountState` varchar(2) DEFAULT NULL COMMENT '金融消费账户状态',
+  `financialConsumeAccountId` varchar(100) NOT NULL COMMENT '金融消费账户ID',
+  `financialConsumeCompany` varchar(100) NOT NULL COMMENT '金融消费公司',
+  `financialConsumeAccountState` varchar(2) NOT NULL COMMENT '金融消费账户状态',
   `creditScoring` int(11) DEFAULT NULL COMMENT '信用评分',
-  `authorize` varchar(2) DEFAULT NULL COMMENT '是否授权',
-  `foundTime` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+  `authorize` varchar(2) NOT NULL COMMENT '是否授权',
+  `foundTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
   `updateTime` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   `info_status` tinyint(2) DEFAULT '1' COMMENT '是否有效',
   `delete_operator` varchar(50) DEFAULT NULL COMMENT '删除操作者',
